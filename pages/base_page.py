b7d1ebed2ee4904ab24960f3963d067e0a37d65e
madelinehn <madeline.hn@gmail.com>
2023-07-13 06:43:46 +0200

    Próba uruchomienia testu#2

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,8 +7,11 @@ class BasePage():
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
-    def field_send_keys(self, selector, value, locator_type=By.XPATH):
-        return self.driver.find_element(locator_type, selector).send_keys(value)
+    def type_in_email(self, email):
+        return self.field_send_keys(self.login_field_xpath, email)
 
-    def click_on_the_element(self, selector, selector_type=By.XPATH):
-        return self.driver.find_element(selector_type, selector).click()
+    def type_in_password(self, password):
+        return self.field_send_keys(self.password_field_xpath, password)
+
+    def click_on_the_sign_in_button(self):
+        return self.click_on_the_element(self.sign_in_button_xpath)
--- pages/login_page.py
+++ pages/login_page.py
@@ -11,10 +11,11 @@ class LoginPage(BasePage):
     header_of_box = "Scouts Panel"
 
     def type_in_email(self, email):
-        self.field_send_keys(self.login_field_xpath, email)
+        return self.field_send_keys(self.login_field_xpath, email)
 
     def type_in_password(self, password):
-        self.field_send_keys(self.login_field_xpath, password)
+        return self.field_send_keys(self.password_field_xpath, password)
 
-    def click_on_the_sign_in_button(self.sign_in_button_xpath)
+    def click_on_the_sign_in_button(self):
+        return self.click_on_the_element(self.sign_in_button_xpath)
 
